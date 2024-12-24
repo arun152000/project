7893945fafde2e2cff41d7686ee585fd654b0a11
arun152000 <arunabi1000@gmail.com>
2024-12-24 21:14:58 +0530

    drag project

---
+++ Annan/src/test/java/arun/Annan/Draganddropby.java
@@ -0,0 +1,25 @@
+package arun.Annan;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Draganddropby {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.leafground.com/drag.xhtml");
+		driver.manage().window().maximize();
+		WebElement obj = driver.findElement(By.id("form:conpnl_header"));
+		Actions as=new Actions(driver);
+		Thread.sleep(4000);
+		as.dragAndDropBy(obj, 200, 0).perform();
+		Thread.sleep(4000);
+
+		
+
+	}
+
+}
