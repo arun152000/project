6c2d8ebd92abb29b0563ecfeaedfb6e05fa060c5
arun152000 <arunabi1000@gmail.com>
2024-12-25 18:34:03 +0530

    updated new changes

--- Annan/src/test/java/arun/Annan/Mainproject.java
+++ Annan/src/test/java/arun/Annan/Mainproject.java
@@ -1,9 +1,13 @@
 package arun.Annan;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
 
 public class Mainproject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		WebDriver driver=new ChromeDriver();
+		
 
 	}
 
