37d17e7a8fca49d9ce28f6688f4948f971ac5260
arun152000 <arunabi1000@gmail.com>
2025-01-01 20:19:25 +0530

    add new task

---
+++ Annan/src/test/java/arun/Annan/Task12.java
@@ -0,0 +1,35 @@
+package arun.Annan;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Task12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	    driver.navigate().to("http://leaftaps.com/opentaps/control/main");
+	    driver.findElement(By.xpath("//input[@id=\"username\"]")).sendKeys("DemoSalesManager");
+	    driver.findElement(By.xpath("//input[@id=\"password\"]")).sendKeys("crmsfa");
+	    driver.findElement(By.cssSelector(".decorativeSubmit")).click();
+	    driver.findElement(By.linkText("CRM/SFA")).click();
+	    String Homepage=driver.getTitle();
+	    System.out.println(Homepage);
+	    WebElement ak=driver.findElement(By.xpath("//b[text()=\"DemoSalesManager\"]"));
+		String actual=ak.getText();
+		String expec="DemoSalesManager";
+	    if(actual.equalsIgnoreCase(expec)) {
+	    	System.out.println("passed");
+	    	driver.close();
+	    }
+	    else {
+	    	System.out.println("failed");
+	    }
+	    
+
+	}
+
+}
