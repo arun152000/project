51c8ce6d862f761d479e855866826381242801e7
arun152000 <arunabi1000@gmail.com>
2025-01-01 19:41:09 +0530

    add new task

---
+++ Annan/src/test/java/arun/Annan/Task11.java
@@ -0,0 +1,32 @@
+package arun.Annan;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Task11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	    driver.navigate().to("https://practicetestautomation.com/practice-test-login/");
+	    String Actual=driver.getTitle();
+	    System.out.println(Actual);
+	    String Actualurl=driver.getCurrentUrl();
+	    String Expected="https://practicetestautomation.com/practice-test-login/";
+	    if(Actualurl.equalsIgnoreCase(Expected)) {
+	    	
+	    	driver.findElement(By.cssSelector("#username")).sendKeys("student");
+	 	    driver.findElement(By.cssSelector("input[type=\"password\"][name=\"password\"]")).sendKeys("incorrectPassword");
+	 	    driver.findElement(By.cssSelector(".btn")).click();
+	 	    String Text=driver.findElement(By.cssSelector(".show")).getText();
+	 	    System.out.println(Text);
+	 	    driver.close();
+	 	    
+	    }
+	    else {
+	    	System.out.println("failed");
+	    }
+	}
+}
